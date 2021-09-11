ddda1e6574d961ba5b72830af95bef3fed9e6f59
P3NG00 <p3ng00minecraft@gmail.com>
2021-09-10 22:17:36 -0600

    GameInfo SpaceRock variables renamed

--- Assets/Scripts/Settings/GameModeSettings.cs
+++ Assets/Scripts/Settings/GameModeSettings.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SpaceGame.Settings
 {
@@ -6,40 +7,40 @@ namespace SpaceGame.Settings
     public sealed class GameModeSettings : ScriptableObject
     {
         [Header("Space Rocks", order = 40)]
-        [SerializeField, Min(0f)] private float minVelocity;
-        [SerializeField] private float maxVelocity;
-        [SerializeField, Min(0f)] private float minAngularVelocity;
-        [SerializeField] private float maxAngularVelocity;
-        [SerializeField, Min(0f)] private float minScale;
-        [SerializeField] private float maxScale;
-        [SerializeField] private float scaleSpawnRate;
-        [SerializeField] private float distanceSpawn;
-        [SerializeField] private float distanceMax;
-        [SerializeField] private float timeBetweenCleanup;
-        [SerializeField, Min(0f)] private float timeBetweenChance;
-        [SerializeField, Range(0f, 1f)] private float chanceSpawn;
-        [SerializeField, Min(0f)] private float scaleMissileImpactForce;
+        [SerializeField, Min(0f)] private float sr_minVelocity;
+        [SerializeField] private float sr_maxVelocity;
+        [SerializeField, Min(0f)] private float sr_minAngularVelocity;
+        [SerializeField] private float sr_maxAngularVelocity;
+        [SerializeField, Min(0f)] private float sr_minScale;
+        [SerializeField] private float sr_maxScale;
+        [SerializeField] private float sr_scaleSpawnRate;
+        [SerializeField] private float sr_distanceSpawn;
+        [SerializeField] private float sr_distanceMax;
+        [SerializeField] private float sr_timeBetweenCleanup;
+        [SerializeField, Min(0f)] private float sr_timeBetweenChance;
+        [SerializeField, Range(0f, 1f)] private float sr_chanceSpawn;
+        [SerializeField, Min(0f)] private float sr_scaleMissileImpactForce;
 
         private void OnValidate()
         {
             void ValidateMinMax(float min, ref float max) { if (min > max) { max = min; } }
-            ValidateMinMax(minVelocity, ref maxVelocity);
-            ValidateMinMax(minAngularVelocity, ref maxAngularVelocity);
-            ValidateMinMax(minScale, ref maxScale);
+            ValidateMinMax(sr_minVelocity, ref sr_maxVelocity);
+            ValidateMinMax(sr_minAngularVelocity, ref sr_maxAngularVelocity);
+            ValidateMinMax(sr_minScale, ref sr_maxScale);
         }
 
-        public float MinSpaceRockVelocity => minVelocity;
-        public float MaxSpaceRockVelocity => maxVelocity;
-        public float MinSpaceRockVelocityAngular => minAngularVelocity;
-        public float MaxSpaceRockVelocityAngular => maxAngularVelocity;
-        public float MinSpaceRockScale => minScale;
-        public float MaxSpaceRockScale => maxScale;
-        public float ScaleSpaceRockSpawnRate => scaleSpawnRate;
-        public float DistanceSpaceRockSpawn => distanceSpawn;
-        public float DistanceSpaceRockMax => distanceMax;
-        public float TimeBetweenSpaceRockCleanup => timeBetweenCleanup;
-        public float TimeBetweenSpaceRockChance => timeBetweenChance;
-        public float ChanceSpaceRockSpawn => chanceSpawn;
-        public float ScaleMissileImpactForce => scaleMissileImpactForce;
+        public float MinSpaceRockVelocity => sr_minVelocity;
+        public float MaxSpaceRockVelocity => sr_maxVelocity;
+        public float MinSpaceRockVelocityAngular => sr_minAngularVelocity;
+        public float MaxSpaceRockVelocityAngular => sr_maxAngularVelocity;
+        public float MinSpaceRockScale => sr_minScale;
+        public float MaxSpaceRockScale => sr_maxScale;
+        public float ScaleSpaceRockSpawnRate => sr_scaleSpawnRate;
+        public float DistanceSpaceRockSpawn => sr_distanceSpawn;
+        public float DistanceSpaceRockMax => sr_distanceMax;
+        public float TimeBetweenSpaceRockCleanup => sr_timeBetweenCleanup;
+        public float TimeBetweenSpaceRockChance => sr_timeBetweenChance;
+        public float ChanceSpaceRockSpawn => sr_chanceSpawn;
+        public float ScaleMissileImpactForce => sr_scaleMissileImpactForce;
     }
 }
